0013311ca2bf5364e470e82f04dfccae60fc611b
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2024-11-09 16:41:37 +0000

    Tokenize prompt

--- src/icp_hello_world_rust_frontend/src/index.js
+++ src/icp_hello_world_rust_frontend/src/index.js
@@ -1,16 +1,29 @@
 import { icp_gpt2 } from "../../declarations/icp_gpt2";
+import { encode } from 'gpt-tokenizer'
 
 
 document.querySelector("form").addEventListener("submit", async (e) => {
+
+    // Button
     e.preventDefault();
     const button = e.target.querySelector("button");
-    const name = document.getElementById("name").value.toString();
     button.setAttribute("disabled", true);
 
-    const answer = await icp_gpt2.model_inference(14, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+    // Prompt
+    const prompt = document.getElementById("name").value.toString();
 
+    // Tokenize
+    const text = 'Hello, world!'
+    const tokens = encode(text)
+    console.log('tokens', tokens)
+
+    // Get answer
+    const answer = await icp_gpt2.model_inference(tokens.length, tokens);
+
+    // Enable button
     button.removeAttribute("disabled");
 
+    // Display message
     console.log(answer)
     // document.getElementById("greeting").innerText = answer;
  
