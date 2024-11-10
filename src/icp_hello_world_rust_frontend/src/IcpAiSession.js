0e2f8884e5f32b32b64f38a48bcb6acec7507a0e
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2024-11-10 16:37:48 +0000

    Workaround for model boundaries: prompt loops

--- src/icp_hello_world_rust_frontend/src/IcpAiSession.js
+++ src/icp_hello_world_rust_frontend/src/IcpAiSession.js
@@ -4,11 +4,15 @@ import { encode, decode } from 'gpt-tokenizer/encoding/p50k_base';
 
 export default class IcpAiSession {
 
-    constructor() {
+    constructor(args) {
+
         this.session = {
-            progress: [],
-            final: []
+            prompt: [],
+            result: []
         };
+        this.final = [];
+        this.maxTokens = ('maxTokens' in args) ? args.maxTokens : 15;
+
     }
 
     async prompt(text) {
@@ -22,7 +26,7 @@ export default class IcpAiSession {
         // Call model
         let answer = {};
         try {
-            answer = await icp_gpt2.model_inference(18, tokens);
+            answer = await icp_gpt2.model_inference(this.maxTokens, tokens);
             console.log('answer:', answer);
         }
         catch(err) {
@@ -33,13 +37,51 @@ export default class IcpAiSession {
         // Decode answer
         if ('Ok' in answer) {
             const response = decode(answer.Ok);
-            console.log('decoded:', response);
-            this.session.progress.push({prompt: tokens, result: Array.from(answer.Ok, bigInt => Number(bigInt))});
+            console.log('decoded:', response.trim());
+            this.session.prompt = tokens;
+            this.session.result = Array.from(answer.Ok, bigInt => Number(bigInt));
+            this.final.push(...this.session.result);
             console.log('session:', this.session);
             return response;
         }
 
         return null;
+    }
+
+    async next() {
+
+        // Decrease max tokens
+        if (this.maxTokens > 1) this.maxTokens = this.maxTokens - 1;
+
+        // Concat prompt + result
+        this.session.prompt.push(...this.session.result);
+        console.log('tokens:', this.session.prompt);
+
+        // Call model
+        let answer = {};
+        try {
+            answer = await icp_gpt2.model_inference(this.maxTokens, this.session.prompt);
+            console.log('answer:', answer);
+        }
+        catch(err) {
+            console.error(err);
+            return null;
+        }
+
+        // Decode answer
+        if ('Ok' in answer) {
+            this.session.result = Array.from(answer.Ok, bigInt => Number(bigInt));
+            this.final.push(...this.session.result);
+            console.log('decoded:', decode(answer.Ok).trim());
+            console.log('session:', this.session);
+        }
+
+        return null;
+    }
+
+    answer() {
+        const response = decode(this.final);
+        return response.trim();
     }
 
 }
--- src/icp_hello_world_rust_frontend/src/index.js
+++ src/icp_hello_world_rust_frontend/src/index.js
@@ -8,19 +8,26 @@ document.querySelector('form').addEventListener('submit', async (e) => {
     const button = e.target.querySelector('button');
     button.setAttribute('disabled', true);
 
-    // Get prompt text
+    // Get inputs
     const prompt = document.getElementById('name').value.toString();
+    const maxtokens = parseInt(document.getElementById('max_tokens').value);
+    const loops = parseInt(document.getElementById('loops').value);
 
     // Prompt AI
-    const ai = new IcpAiSession();
-    const answer = await ai.prompt(prompt);
+    const ai = new IcpAiSession({maxTokens: maxtokens});
+    console.log('Pass 1', '-------------------');
+    await ai.prompt(prompt);
+
+    for (let x = 0; x < loops - 1; x ++) {
+        console.log('Pass', x + 2, '-------------------');
+        await ai.next();
+    }
     
     // Enable button
     button.removeAttribute('disabled');
 
     // Display message
-    if (answer) document.getElementById('greeting').innerText = answer;
-    else document.getElementById('greeting').innerText = 'Model error';
+    document.getElementById('greeting').innerText = ai.answer();
  
     return false;
 });
